0a89695852b0501549a4cb27caa1d35d4b51cf9b
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-08-20 13:28:24 +0400

    daae

--- BookStoreCatalog/BookPrice.cs
+++ BookStoreCatalog/BookPrice.cs
@@ -5,40 +5,74 @@ namespace BookStoreCatalog
     /// <summary>
     /// Represents a book price.
     /// </summary>
-    // TODO Add class declaration.
+    public class BookPrice
     {
-        // TODO Add fields.
+        private decimal amount;
+        private string currency;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="BookPrice"/> class.
         /// </summary>
-        // TODO Add constructor.
+        public BookPrice()
+            : this(0.0m, "USD")
+        {
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="BookPrice"/> class with specified <paramref name="amount"/> and <paramref name="currency"/>.
         /// </summary>
         /// <param name="amount">An amount of money of a book.</param>
         /// <param name="currency">A price currency.</param>
-        // TODO Add constructor.
+        public BookPrice(decimal amount, string currency)
+        {
+            ThrowExceptionIfAmountIsNotValid(amount, nameof(amount));
+            ThrowExceptionIfCurrencyIsNotValid(currency, nameof(currency));
+            this.amount = amount;
+            this.currency = currency;
+        }
 
         /// <summary>
         /// Gets or sets an amount of money that a book costs.
         /// </summary>
-        // TODO Add property.
+        public decimal Amount
+        {
+            get => this.amount;
+            set
+            {
+                ThrowExceptionIfAmountIsNotValid(value, nameof(value));
+                this.amount = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets a book price currency.
         /// </summary>
-        // TODO Add property.
+        public string Currency
+        {
+            get => this.currency;
+            set
+            {
+                ThrowExceptionIfCurrencyIsNotValid(value, nameof(value));
+                this.currency = value;
+            }
+        }
 
         /// <summary>
         /// Returns the string that represents a current object.
         /// </summary>
         /// <returns>A string that represents the current object.</returns>
-        // TODO Add method.
+        public new string ToString() => string.Format(CultureInfo.InvariantCulture, "{0:N2} {1}", this.amount, this.currency);
 
-        // TODO Add method.
+        private static void ThrowExceptionIfAmountIsNotValid(decimal amount, string value) => _ = amount < 0 ? throw new ArgumentException("Amount cant be less then 0.", value) : amount;
 
-        // TODO Add method.
+        private static void ThrowExceptionIfCurrencyIsNotValid(string currency, string value)
+        {
+            _ = currency ?? throw new ArgumentNullException(value, $"Can't be empty or null.");
+
+            if (currency.Length != 3 || currency.Any(c => !char.IsLetter(c)))
+            {
+                throw new ArgumentException("Invalid currency.", value);
+            }
+        }
     }
 }
